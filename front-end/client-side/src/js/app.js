60c0d61c257bf349c33318183fd78032281e78fb
ecDusty <e.macalligan@outlook.com>
2018-10-20 12:38:40 +0800

    ADD: Client-side & Business-side starts created

---
+++ front-end/client-side/src/js/app.js
@@ -0,0 +1,18 @@
+/* global google, ko, console */
+
+// Custom Code for application build
+
+
+// KNOCKOUT CODING SECTION
+//
+
+
+//Menu Items
+//
+
+
+function etecaViewModel() {
+    const self = this;
+
+}
+ko.applyBindings(new etecaViewModel());
