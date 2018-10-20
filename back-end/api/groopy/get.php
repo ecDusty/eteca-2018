0de2fac4d9b4b6dbcde330f865741461b43488dc
Adam Chan <adam@Adams-MacBook-Air.local>
2018-10-21 01:47:26 +0800

    groopy class WIP

---
+++ back-end/api/groopy/get.php
@@ -0,0 +1,42 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+ 
+include_once '../config/database.php';
+include_once '../objects/groopy.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$groopy = new Groopy($db);
+
+$groopy->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+$groopy->get();
+ 
+if($groopy->name!=null) {
+
+    $groopy_arr = array(
+        "id" =>  $groopy->id,
+        "name" => $groopy->name,
+        "cuisineType" => explode(',', $groopy->cuisine),
+        "offer" => $groopy->offer,
+        "timeStart" => date_format(date_create($groopy->time_start), 'H:ia'),
+        "timeEnd" => date_format(date_create($groopy->time_end), 'H:ia'),
+        "peopleTotal" => $groopy->max_pp,
+        "peopleMin" => $groopy->min_pp,
+        "peopleJoining" => $groopy->cur_pp,
+        "needHelp" => $groopy->has_cfh
+    );
+ 
+    http_response_code(200);
+    echo json_encode($groopy_arr);
+} else {
+    http_response_code(404);
+    echo json_encode(array("message" => "Groopy does not exist."));
+}
+?>
---
+++ back-end/api/objects/groopy.php
@@ -0,0 +1,121 @@
+<?php
+class Groopy{
+ 
+    private $conn;
+    private $table_name = "grpy_groopys";
+ 
+    public $id;
+    public $r_id; // restaurant id
+    public $name; // restaurant name
+    public $cuisine; // cuisine type
+    public $time_start;
+    public $time_end;
+    public $max_pp;
+    public $min_pp;
+    public $offer;
+=
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    function get() {
+        $query = "SELECT
+                    count(j.id) as cur_pp, count(cfh) as chf_count, r.name, r.address, r.cuisine, g.id, g.r_id, g.time_start, g.time_end, g.time_cutoff, g.max_pp, g.min_pp, g.offer
+                FROM
+                    " . $this->table_name . " p
+                LEFT JOIN
+                    grpy_restaurants r
+                        ON g.r_id = r.id
+                LEFT JOIN
+                    grpy_joins j
+                        ON j.r_id = r.id
+                LEFT JOIN
+                    grpy_joins cfh
+                        ON cfh.r_id = r.id
+                WHERE
+                    g.id = ?
+                    AND cfh.status = 'cfh'
+                LIMIT
+                    0,1";
+
+        $stmt = $this->conn->prepare( $query );
+
+        $stmt->bindParam(1, $this->id);
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->id           = $row['id'];
+        $this->name         = $row['name'];
+        $this->cuisine      = $row['cuisine'];
+        $this->time_start   = $row['time_start'];
+        $this->time_end     = $row['time_end'];
+        $this->time_cutoff  = $row['time_cutoff'];
+        $this->max_pp       = $row['max_pp'];
+        $this->min_pp       = $row['min_pp'];
+        $this->offer        = $row['offer'];
+        $this->cur_pp       = $row['cur_pp'];
+        $this->has_cfh      = $row['cfh_count'] > 0 ? true : false;
+    }
+
+    // create user
+    function create() {
+    
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    name=:name, email=:email, password=:password";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->email=htmlspecialchars(strip_tags($this->email));
+        
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+        
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+    }
+
+    function authenticate() {
+        $query = "SELECT
+                    p.id, p.email, p.password
+                FROM
+                    " . $this->table_name . "
+                WHERE
+                    p.email=:email
+                LIMIT
+                    0,1";
+        
+        $stmt = $this->conn->prepare( $query );
+
+        $stmt->bindParam(":email", $this->email);
+
+        $stmt->execute();
+
+        $num->stmt->rowCount();
+
+        if ($num > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $res = password_verify($this->password, $row['password']);
+            if ($res) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            // user doesn't exist
+            return -1;
+        }
+    }
+}
