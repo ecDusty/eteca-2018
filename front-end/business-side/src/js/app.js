015324cae53b275f8ead656b374354bfa84e263a
ecDusty <e.macalligan@outlook.com>
2018-10-20 14:40:17 +0800

    ADD: MODEL Get, Post, Put, Delete

--- front-end/business-side/src/js/app.js
+++ front-end/business-side/src/js/app.js
@@ -1,32 +1,3 @@
 /* global google, ko, console */
 
 // Custom Code for application build
-class Model = {
-    constructor = {
-        this.Name = `Groopy App`;
-    }
-
-    get(url) {
-        return fetch(url).then(response => response.json())
-    }
-
-}
-
-// KNOCKOUT CODING SECTION
-//
-class etecaViewModel() {
-
-    initApp() {
-
-    }
-}
-
-//Menu Items
-//
-
-
-function etecaViewModel() {
-    const self = this;
-
-}
-ko.applyBindings(new etecaViewModel());
--- front-end/client-side/src/js/app.js
+++ front-end/client-side/src/js/app.js
@@ -1,18 +1,88 @@
 /* global google, ko, console */
 
 // Custom Code for application build
+class Modelgroopy {
+    constructor() {
+        this.Name = `groopy App`;
+    }
 
+    get(url) {
+        return fetch(url).then(response => response.json());
+    }
+
+    postG(url,options) {
+        console.log(`Posting object:
+                    ${Options}`);
+        return fetch(url,{
+            method: `post`,
+            body: JSONstringify(options)
+        }).then(response => response.json())
+    }
+
+    putG(url,options) {
+        console.log(`Putting object:
+                    ${Options}`);
+        return fetch(url,{
+            method: `post`,
+            body: JSONstringify(options)
+        }).then(response => response.json())
+    }
+
+    deleteG(url,options) {
+        console.log(`Deleting object:
+                    ${Options}`);
+        return fetch(url,{
+            method: `post`,
+            body: JSONstringify(options)
+        }).then(response => response.json())
+    }
+}
 
 // KNOCKOUT CODING SECTION
 //
-
 
 //Menu Items
 //
 
 
-function etecaViewModel() {
-    const self = this;
+class ClientgroopyViewModel {
+    constructor() {
+        //Pull In Model
+        this.M = new Modelgroopy();
 
+        //Main Menu Structure
+        this.activeMenu = ko.observable('');
+        this.MenuItem = function(name,imageURL,CSSclass) {
+            this.name = name;
+            this.url = imageURL;
+            this.alt = `${this.name} Icon`;
+            this.CSSclass = ko.observable(CSSclass);
+            this.active = ko.observable(false);
+        }
+        this.mainMenuItems = [
+            new self.MenuItem('groopys'),
+            new self.MenuItem('My groopys'),
+            new self.MenuItem('Profile')
+        ]
+
+    }
+
+
+    //Menu Functions structure
+    //
+    menuClick(item) {
+        if (item.active()) 
+            item.active(false);
+        else {
+            for (const mItem of self.mainMenuItems) {
+                mItem.active(false);
+            }
+            item.active(true);
+        }
+    }
+
+    testClass() {
+        console.log('Testing ES6 Class structure')
+    }
 }
-ko.applyBindings(new etecaViewModel());
+ko.applyBindings(new ClientViewModel());
