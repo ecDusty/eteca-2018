bf76d67aa60527c8154db6e322d395d6c7e4faa8
ecDusty <e.macalligan@outlook.com>
2018-10-20 12:03:37 +0800

    START: Hackathon Start - Front End Source code structure

---
+++ front-end/src/js/app.js
@@ -0,0 +1,16 @@
+/* global google, ko, console */
+
+// Custom Code for application build
+
+
+// KNOCKOUT CODING SECTION
+//
+
+//Menu Items
+//
+
+function etecaViewModel() {
+    const self = this;
+
+}
+ko.applyBindings(new etecaViewModel());
